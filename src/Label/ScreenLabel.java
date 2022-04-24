a8f545321c51ada78725993be15a8cee7278bb28
level237 <Bramslevel129@gmail.com>
2022-04-24 10:15:48 +0100

    import and adding Police in the label

--- src/Label/ScreenLabel.java
+++ src/Label/ScreenLabel.java
@@ -1,12 +1,16 @@
 package Label;
 
+import java.awt.Font;
+
 import javax.swing.JLabel;
 
 public class ScreenLabel extends JLabel {
-
+	
+	private Font police;
 	public ScreenLabel() {
 		// TODO Auto-generated constructor stub
 		this.setText("0");
+		
 	}
 
 }
--- src/Panel/ScreenPanel.java
+++ src/Panel/ScreenPanel.java
@@ -6,5 +6,6 @@ public class ScreenPanel extends JPanel{
 
 	public ScreenPanel() {
 		
+		
 	}
 }
