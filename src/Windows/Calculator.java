19651acb6d5f6aa343b7a164e5918b38da9062dc
level237 <Bramslevel129@gmail.com>
2022-04-24 05:46:55 +0100

    init JFrame librairies

--- src/Windows/Calculator.java
+++ src/Windows/Calculator.java
@@ -1,6 +1,8 @@
 package Windows;
 
-public class Calculator {
+import javax.swing.JFrame;
+
+public class Calculator extends JFrame{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
