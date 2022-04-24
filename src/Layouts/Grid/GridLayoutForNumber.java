0d35cca6cae1dbbab56fc71a7cbf8d7563ce2868
level237 <Bramslevel129@gmail.com>
2022-04-24 16:07:37 +0100

    add rows and columns

--- src/Layouts/Grid/GridLayoutForNumber.java
+++ src/Layouts/Grid/GridLayoutForNumber.java
@@ -4,8 +4,12 @@ import java.awt.GridLayout;
 
 public class GridLayoutForNumber extends GridLayout{
 
-	public GridLayoutForNumber() {
+	public GridLayoutForNumber(int columns,int rows) {
 		// TODO Auto-generated constructor stub
+		this.setColumns(columns);
+		this.setHgap(5);
+		this.setRows(rows);
+		this.setVgap(5);
 	}
 
 }
