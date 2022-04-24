86103bf03a7d65cc8796a0d48784d32df4bb3500
level237 <Bramslevel129@gmail.com>
2022-04-24 10:32:41 +0100

    added label dimension in the ScreenPanel

--- src/Panel/ScreenPanel.java
+++ src/Panel/ScreenPanel.java
@@ -1,11 +1,13 @@
 package Panel;
 
+import java.awt.Dimension;
+
 import javax.swing.JPanel;
 
 public class ScreenPanel extends JPanel{
 
 	public ScreenPanel() {
 		
-		
+		this.setPreferredSize(new Dimension(280,40));
 	}
 }
