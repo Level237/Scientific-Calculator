17aa05c7b1709ac4f0280ae0bdfa97b61ac8f985
level237 <Bramslevel129@gmail.com>
2022-04-24 14:17:37 +0100

    init Component GroupButton

---
+++ src/Components/button/List/GroupButton.java
@@ -0,0 +1,16 @@
+package Components.button.List;
+
+import Components.button.NumberButton;
+
+public class GroupButton{
+
+	
+	private String[] symbol={ "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ".", "/", "=", "-.", "+", "-", "x", "Pi",
+			"Cos", "Sin", "Tan", "Ln", "exp", "off", "e", "racine","C"};
+	private NumberButton[] groupButton=new NumberButton[symbol.length];
+	
+	public GroupButton() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
