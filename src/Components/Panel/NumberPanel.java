9d4ab8d24f2a4a0191014b351c196354874a75a1
level237 <Bramslevel129@gmail.com>
2022-04-24 15:53:07 +0100

    Dimension of the numberPanel

--- src/Components/Panel/NumberPanel.java
+++ src/Components/Panel/NumberPanel.java
@@ -1,11 +1,14 @@
 package Components.Panel;
 
+import java.awt.Dimension;
+
 import javax.swing.JPanel;
 
 public class NumberPanel extends JPanel{
 
 	public NumberPanel() {
 		// TODO Auto-generated constructor stub
+		this.setPreferredSize(new Dimension(90, 350));
 	}
 
 }
