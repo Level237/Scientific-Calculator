6d8955372756978b9a1ebc131a8993f3f75e93c7
level237 <Bramslevel129@gmail.com>
2022-04-24 10:51:09 +0100

    init Component ScreenPanel:init

--- src/Components/Panel/ScreenPanel.java
+++ src/Components/Panel/ScreenPanel.java
@@ -1,9 +1,16 @@
 package Components.Panel;
 
-public class ScreenPanel {
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+
+public class ScreenPanel extends JPanel{
 
 	public ScreenPanel() {
-		// TODO Auto-generated constructor stub
+		
+		this.setPreferredSize(new Dimension(280,40));
+		this.setBorder(BorderFactory.createLineBorder(Color.black));
 	}
-
 }
