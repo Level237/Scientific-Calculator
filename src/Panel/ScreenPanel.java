bf5ec8a06c636c5927132df46e9286638052f6b1
level237 <Bramslevel129@gmail.com>
2022-04-24 10:37:37 +0100

    added the border to the panel

--- src/Panel/ScreenPanel.java
+++ src/Panel/ScreenPanel.java
@@ -1,7 +1,9 @@
 package Panel;
 
+import java.awt.Color;
 import java.awt.Dimension;
 
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
 public class ScreenPanel extends JPanel{
@@ -9,5 +11,6 @@ public class ScreenPanel extends JPanel{
 	public ScreenPanel() {
 		
 		this.setPreferredSize(new Dimension(280,40));
+		this.setBorder(BorderFactory.createLineBorder(Color.black));
 	}
 }
