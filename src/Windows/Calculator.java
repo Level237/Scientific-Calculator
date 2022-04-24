802cfa0d7a6f542f0265ec2a0ceba5be440e2787
level237 <Bramslevel129@gmail.com>
2022-04-24 09:40:49 +0100

    init constructor

--- src/Windows/Calculator.java
+++ src/Windows/Calculator.java
@@ -3,6 +3,10 @@ package Windows;
 import javax.swing.JFrame;
 
 public class Calculator extends JFrame{
+	
+	public Calculator() {
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
