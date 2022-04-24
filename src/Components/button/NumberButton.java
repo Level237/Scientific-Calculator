68e3213161d7a88bbdbe4f719d15daa4b5ae2fd8
level237 <Bramslevel129@gmail.com>
2022-04-24 14:10:40 +0100

    init Component Button

--- src/Components/button/NumberButton.java
+++ src/Components/button/NumberButton.java
@@ -1,9 +1,17 @@
 package Components.button;
 
-public class NumberButton {
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+
+public class NumberButton extends JButton{
 
 	public NumberButton() {
 		// TODO Auto-generated constructor stub
+		this.setBackground(Color.black);
+		this.setPreferredSize(new Dimension(40,30));
+		this.setForeground(Color.white);
 	}
 
 }
