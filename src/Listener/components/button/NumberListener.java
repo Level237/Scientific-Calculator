120a326477884860b64046000ba9e6d2009a05d5
level237 <Bramslevel129@gmail.com>
2022-05-22 16:13:20 +0200

    import button'listener

--- src/Listener/components/button/NumberListener.java
+++ src/Listener/components/button/NumberListener.java
@@ -5,15 +5,21 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import Components.Label.ScreenLabel;
+import Components.button.NumberButton;
 
 public class NumberListener implements ActionListener{
+	
 	private ScreenLabel screen;
-	public NumberListener(ScreenLabel screen) {
+	private NumberButton button;
+	
+	public NumberListener(ScreenLabel screen,NumberButton button) {
 		this.screen=screen;
+		this.button=button;
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		String
 			this.screen.setForeground(Color.black);
 	}
 
