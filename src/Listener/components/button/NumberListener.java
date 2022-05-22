3bf61bdc6767c848fc0363c437247c071c4673c0
level237 <Bramslevel129@gmail.com>
2022-05-22 16:02:03 +0200

    implements ActionListener

--- src/Listener/components/button/NumberListener.java
+++ src/Listener/components/button/NumberListener.java
@@ -1,5 +1,14 @@
 package Listener.components.button;
 
-public class NumberListener {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class NumberListener implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+			
+	}
 
 }
