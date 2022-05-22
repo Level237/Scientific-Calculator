7ac977fca4f123a57cfd8f798128bbc42f28594d
level237 <Bramslevel129@gmail.com>
2022-05-22 19:36:38 +0200

    update

--- src/Listener/components/button/EqualListener.java
+++ src/Listener/components/button/EqualListener.java
@@ -1,9 +1,21 @@
 package Listener.components.button;
 
-public class EqualListener {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import Components.Label.ScreenLabel;
+
+public class EqualListener  extends ListenerButtonImplement implements ActionListener{
+	
+	private ScreenLabel screen;
+	private OperatorArithmetic operator;
 	public EqualListener() {
 		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
